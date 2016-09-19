b6e389cfb200bec251189e66a9fc4d900acf3aac
300621334 <shafiqu@my.centennialcollege.ca>
2016-09-19 17:41:46 -0400

    initial commit

---
+++ AccountsDemo/Accounts.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsDemo
+{
+    class Accounts
+    {
+        //fields
+
+        //A readonly field is like a consts but it is normally assigned when the program is running
+        //WHile a const is a value that cannot be modified.Its value is set at declaration.i.e.When the program is compiled
+        private static int CURRENT_ACCOUNT_NUMBER;
+        private const int TRANSIT_NUMBER = 314;  //cont is static by default :-)
+        public readonly string Number;
+
+        //properties
+        public double Balance { get; private set; }
+        public List<string> Names { get; private set; }
+
+        //Two Constructor
+        //static constructor can't be done "new". It triggers even when we instantiate the Class with a different non-static constructor.
+        //auto-instantiates
+        static Accounts()
+        {
+            CURRENT_ACCOUNT_NUMBER = 1000;
+        }
+
+        private Accounts(string number, string name, double balance)
+        {
+            Number = number;
+            Balance = balance;
+            Names = new List<string>();
+            Names.Add(name);
+        }
+
+        //ToString Override
+        public override string ToString()
+        {
+            return " A/C#  "+Number+" Balance "+Balance;
+        }
+
+        //Methods
+        public void AddName(string name)
+        {
+            Names.Add(name);
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        public static Accounts CreateAccounts(string name, double balance = 0)
+        {
+            string fullAccNumber = "AC-"+TRANSIT_NUMBER +"-"+ CURRENT_ACCOUNT_NUMBER;
+            CURRENT_ACCOUNT_NUMBER++;
+            return new Accounts(fullAccNumber, name, balance);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ AccountsDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Accounts> accounts = new List<Accounts>();
+            Random rand = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                accounts.Add(Accounts.CreateAccounts("#" + i, rand.Next(10, 100)));
+            }
+
+            Console.WriteLine("\n\nAll Accounts");
+            foreach (Accounts Accounts in accounts)
+            {
+                Console.WriteLine(Accounts);
+            }
+
+            foreach (Accounts Accounts in accounts)
+            {
+                Accounts.Deposit(1.11);
+            }
+
+            Console.WriteLine("\n\nAfter $1.11 deposit ");
+            foreach (Accounts Accounts in accounts)
+            {
+                Console.WriteLine(Accounts);
+            }
+
+            foreach (Accounts Accounts in accounts)
+            {
+                Accounts.Withdraw(0.05);
+            }
+
+            Console.WriteLine("\n\nAfter $0.05 withdrawal.");
+            foreach (Accounts Accounts in accounts)
+            {
+                Console.WriteLine(Accounts);
+            }
+
+        }
+    }
+}
